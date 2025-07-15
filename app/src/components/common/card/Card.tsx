cbd2a143a5211223a2f51a333191adfa35625bc8
Agathe Cazals <agathe.cazals.pro@gmail.com>
2025-07-15 13:33:31 +0200

    style(Card): fix title size

--- app/src/components/common/card/Card.tsx
+++ app/src/components/common/card/Card.tsx
@@ -12,7 +12,7 @@ export const Card = ({ iconPath, title, altIcon, children }: CardProps) => {
   return (
     <div className="flex flex-col flex-1 gap-4 rounded-xl border border-gray-300 p-4">
       <img src={iconPath} alt={altIcon} className="w-10 h-10 md:w-16 md:h-16" />
-      <Text weight="semibold" className="text-xl">
+      <Text weight="semibold" size="medium">
         {title}
       </Text>
       {children}
